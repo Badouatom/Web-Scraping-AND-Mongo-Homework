d5af0682e254300b108b292200feca70020bd67e
BADOU ARTHUR KOSSONOU <47370403+Badouatom@users.noreply.github.com>
2019-09-20 10:03:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 
